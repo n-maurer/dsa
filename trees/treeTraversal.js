8e5dbeeeb57ab73b8b6ce8cfdaffb53ef6a74343
Noah Maurer <noahmaurer12@gmail.com>
2023-04-24 15:43:34 -0500

    BST

---
+++ trees/treeTraversal.js
@@ -0,0 +1,67 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            var current = this.root;
+            if (value === current.value) return undefined;
+            while (true) {
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    find(value) {
+        var current = this.root;
+        while (true) {
+            if (!current) return undefined;
+            if (value === current.value) {
+                return current;
+            } else if (value < current.value) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+    }
+
+    bfs() {}
+
+    dfs() {}
+}
+
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.insert(7);
+tree.insert(11);
+tree.insert(13);
+console.log(tree.find(1));
+console.log(tree);
